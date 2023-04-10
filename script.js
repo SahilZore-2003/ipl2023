a6f54c02cf84ea3d4b04e68d3f60584727b7d1d0
sahil zore <zoresahil80@gmail.com>
2023-04-10 20:27:28 +0530

    change index 9 to 1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const date = document.getElementById("date");
 const shareBtn =  document.getElementById("shareBtn");
 const live =  document.getElementById("live");
 const onoffstatus =  document.getElementById("onoffstatus");
-const index = 9;
+const index = 1;
 
 window.addEventListener('online',()=>{
    onoffstatus.style.display = "none";
