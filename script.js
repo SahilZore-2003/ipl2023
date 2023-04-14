7d9d7bb7985a0e5495befdfab03c31333c7859b2
sahil zore <zoresahil80@gmail.com>
2023-04-14 09:02:10 +0530

    api updated

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
 const div = document.getElementById("content");
 const date = document.getElementById("date");
-const shareBtn =  document.getElementById("shareBtn");
-const live =  document.getElementById("live");
-const onoffstatus =  document.getElementById("onoffstatus");
+const shareBtn = document.getElementById("shareBtn");
+const live = document.getElementById("live");
+const onoffstatus = document.getElementById("onoffstatus");
 const index = 7;
 
-window.addEventListener('online',()=>{
-   onoffstatus.style.display = "none";
+window.addEventListener('online', () => {
+  onoffstatus.style.display = "none";
 })
 
-window.addEventListener('offline',()=>{
+window.addEventListener('offline', () => {
   onoffstatus.style.display = "block";
 })
 
 
-window.addEventListener("load",()=>{
-    document.getElementById('logo').style.display = 'none';
+window.addEventListener("load", () => {
+  document.getElementById('logo').style.display = 'none';
 })
 
 let displaydate = new Date()
-date.innerHTML = `${displaydate.getDate()}/${displaydate.getMonth()+1}/${displaydate.getFullYear()}`
+date.innerHTML = `${displaydate.getDate()}/${displaydate.getMonth() + 1}/${displaydate.getFullYear()}`
 
 
 // api = https://api.cricapi.com/v1/currentMatches?apikey=c042a1a7-0240-4058-8ebb-1f0f9d00e146&offset=0
 
 {/* line number 50
-   <div class="score">${data.data[index].score[1].r}/${data.data[index].score[1].w}</div>
-       <div class="overs">(${data.data[index].score[1].o})</div>
+   
        */}
 
-fetch('https://api.cricapi.com/v1/currentMatches?apikey=c042a1a7-0240-4058-8ebb-1f0f9d00e146&offset=0')
-.then(res => res.json())
-.then(data => {
-   console.log(data);
-   live.innerHTML = `
-   <div class="flex">
-   <div class="team1">
-       <div class="teamlogo"><img src="${data.data[index].teamInfo[1].img}" alt="">
-       </div>
+// fetch('https://api.cricapi.com/v1/currentMatches?apikey=c042a1a7-0240-4058-8ebb-1f0f9d00e146&offset=0')
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//     live.innerHTML = `
+//    <div class="flex">
+//    <div class="team1">
+//        <div class="teamlogo"><img src="${data.data[index].teamInfo[1].img}" alt="">
+//        </div>
 
-       <div class="name">${data.data[index].teamInfo[1].shortname}</div>
-       <div class="score">${data.data[index].score[0].r}/${data.data[index].score[0].w}</div>
-       <div class="overs">(${data.data[index].score[0].o})</div>
-   </div>
-   <div class="vs">VS</div>
-   <div class="team2">
-       <div class="teamlogo"><img src="${data.data[index].teamInfo[0].img}" alt="">
-       </div>
+//        <div class="name">${data.data[index].teamInfo[1].shortname}</div>
+//        <div class="score">${data.data[index].score[0].r}/${data.data[index].score[0].w}</div>
+//        <div class="overs">(${data.data[index].score[0].o})</div>
+//    </div>
+//    <div class="vs">VS</div>
+//    <div class="team2">
+//        <div class="teamlogo"><img src="${data.data[index].teamInfo[0].img}" alt="">
+//        </div>
 
-       <div class="name">${data.data[index].teamInfo[0].shortname}</div>
-      
+//        <div class="name">${data.data[index].teamInfo[0].shortname}</div>
+//        <div class="score">${data.data[index].score[1].r}/${data.data[index].score[1].w}</div>
+//        <div class="overs">(${data.data[index].score[1].o})</div>
        
 
-   </div>
-</div>
-<div class="result">${data.data[index].status}</div>
-   `
-})
+//    </div>
+// </div>
+// <div class="result">${data.data[index].status}</div>
+//    `
+//   })
 
 
 
 
 
 fetch("data.json")
-.then(res => res.json())
-.then(data =>{
-    for(let i = 0; i<data.matches.length;i++){
-        div.innerHTML += `
+  .then(res => res.json())
+  .then(data => {
+    for (let i = 0; i < data.matches.length; i++) {
+      div.innerHTML += `
         <div class="center matchid">Match ${data.matches[i].match}</div>
         <div class="displayscore">
             <div>
@@ -81,23 +81,22 @@ fetch("data.json")
         <div class="result center">${data.matches[i].result}</div>
         `;
     }
-})
+  })
 
 
 const shareData = {
-    title: "Smart IT World",
-    text: "see ipl matches score schedule & more..",
-    url: "https://sahilzore-2003.github.io/ipl2023",
-  };
-  
- 
-  
-  // Share must be triggered by "user activation"
-  shareBtn.addEventListener("click", async () => {
-    try {
-      await navigator.share(shareData);
-    } catch (err) {
-      console.log(err)
-    }
-  });
-  
+  title: "Smart IT World",
+  text: "see ipl matches score schedule & more..",
+  url: "https://sahilzore-2003.github.io/ipl2023",
+};
+
+
+
+// Share must be triggered by "user activation"
+shareBtn.addEventListener("click", async () => {
+  try {
+    await navigator.share(shareData);
+  } catch (err) {
+    console.log(err)
+  }
+});
